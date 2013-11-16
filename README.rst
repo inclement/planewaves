335e98cbc177e9e0b55ce0c7fa780e86bc3361b9
Alexander Taylor <alexanderjohntaylor@gmail.com>
2013-11-16 15:12:53 +0000

    Added license info

--- README.rst
+++ README.rst
@@ -1,1 +1,9 @@
-A simple kivy app displaying plane waves via GLSL shaders
+PlaneWaves
+==========
+
+PlaneWaves is a simple kivy app displaying plane waves via GLSL shaders. You can click/tap to create wavevector markers and move them around to change the wavevector magnitudes. The superposition of all the wavevectors is displayed - you can see just the intensity, or the complex phase, or the phase modulated by intensity.
+
+License
+=======
+
+The app is under an MIT license (see LICENSE), either my own (main.py and planewave.kv) or that of the kivy project (shaderwidget.py).
--- shaderwidget.py
+++ shaderwidget.py
@@ -1,13 +1,22 @@
-'''
-Plasma Shader
-=============
+# Copyright (c) 2010-2013 Kivy Team and other contributors
 
-This shader example have been taken from http://www.iquilezles.org/apps/shadertoy/
-with some adapation.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 
-This might become a Kivy widget when experimentation will be done.
-'''
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 from kivy.clock import Clock
 from kivy.app import App
@@ -62,10 +71,3 @@ class ShaderWidget(FloatLayout):
         # This is needed for the default vertex shader.
         self.canvas['projection_mat'] = Window.render_context['projection_mat']
 
-
-class PlasmaApp(App):
-    def build(self):
-        return ShaderWidget(fs=plasma_shader)
-
-if __name__ == '__main__':
-    PlasmaApp().run()
