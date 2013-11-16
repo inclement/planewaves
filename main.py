87af15908e43c4a05be0114d752d340a88c869e1
Alexander Taylor <alexanderjohntaylor@gmail.com>
2013-11-16 14:27:12 +0000

    Added wv movement and update

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.graphics import RenderContext
-from kivy.properties import StringProperty, ListProperty, ObjectProperty
+from kivy.properties import StringProperty, ListProperty, ObjectProperty, NumericProperty, ReferenceListProperty
+from kivy.metrics import sp
 
 # This header must be not changed, it contain the minimum information from Kivy.
 header = '''
@@ -41,16 +43,17 @@ void main(void)
 
    float resx = 0.0;
    float resy = 0.0;
+   float max_intensity = 1.0;
 '''
 
 shader_bottom_both = '''
    vec3 rgbcol = hsv2rgb( vec3(atan(resy, resx) / (2.0*3.1416), 1, 1));
 
-   gl_FragColor = vec4( rgbcol.x, rgbcol.y, rgbcol.z, sqrt(resx*resx + resy*resy));
+   gl_FragColor = vec4( rgbcol.x, rgbcol.y, rgbcol.z, sqrt(resx*resx + resy*resy) / max_intensity);
 }
 '''
 shader_bottom_intensity = '''
-   gl_FragColor = vec4( 1.0, 1.0, 1.0, sqrt(resx*resx + resy*resy));
+   gl_FragColor = vec4( 1.0, 1.0, 1.0, sqrt(resx*resx + resy*resy) / max_intensity);
 }
 '''
 shader_bottom_phase = '''
@@ -68,6 +71,18 @@ class ShaderWidget(FloatLayout):
     shader_bottom = StringProperty(shader_bottom_both)
     mode = StringProperty('both')
 
+    def __init__(self, **kwargs):
+        # Instead of using Canvas, we will use a RenderContext,
+        # and change the default shader used.
+        self.canvas = RenderContext()
+
+        # call the constructor of parent
+        # if they are any graphics object, they will be added on our new canvas
+        super(ShaderWidget, self).__init__(**kwargs)
+
+        # We'll update our glsl variables in a clock
+        Clock.schedule_interval(self.update_glsl, 1 / 60.)
+
     def on_mode(self, *args):
         if self.mode == 'both':
             self.shader_bottom = shader_bottom_both
@@ -79,22 +94,19 @@ class ShaderWidget(FloatLayout):
     def on_wavevectors(self, *args):
         shader_mid = ''
         for wv in self.wavevectors:
-            kx, ky = wv
+            kx, ky = wv.k
             shader_mid += ('''
             resx += cos({kx}*x / resolution.x + {ky}*y / resolution.y);
             resy += sin({kx}*x / resolution.x + {ky}*y / resolution.y);
             ''').format(kx=kx, ky=ky)
+        shader_mid += ('''
+        max_intensity = {};
+        ''').format(max(1.0, float(len(self.wavevectors))))
         self.shader_mid = shader_mid
 
     def replace_shader(self, *args):
         self.fs = header + shader_top + self.shader_mid + self.shader_bottom
         
-    def on_touch_down(self, touch):
-        length = min(self.width, self.height)
-        dx = touch.x - self.center_x
-        dy = touch.y - self.center_y
-        self.wavevectors.append((dx / length * 25 * 3.1416, dy / length * 50 * 3.1416))
-
     def __init__(self, **kwargs):
         # Instead of using Canvas, we will use a RenderContext,
         # and change the default shader used.
@@ -125,6 +137,54 @@ class ShaderWidget(FloatLayout):
 class AppLayout(BoxLayout):
     wavevector_layout = ObjectProperty()
     
+class WavevectorMaker(Widget):
+    shader_widget = ObjectProperty()
+    markers = ListProperty([])
+    def on_touch_down(self, touch):
+        if not any([marker.collide_point(*touch.pos) for marker in self.markers]):
+            marker = WvMarker(pos=(touch.pos[0]-sp(20), touch.pos[1]-sp(20)))
+            self.add_widget(marker)
+            marker.recalculate_k()
+            self.markers.append(marker)
+
+            length = min(self.width, self.height)
+            dx = touch.x - self.center_x
+            dy = touch.y - self.center_y
+            self.shader_widget.wavevectors.append(marker)
+        else:
+            super(WavevectorMaker, self).on_touch_down(touch)
+
+    def reset(self, *args):
+        for marker in self.markers:
+            self.remove_widget(marker)
+        self.markers = []
+        self.shader_widget.wavevectors = []
+            
+class WvMarker(Widget):
+    colour = ListProperty([0.8, 0.2, 0.2])
+    touch = ObjectProperty(None, allownone=True)
+    kx = NumericProperty(0.0)
+    ky = NumericProperty(0.0)
+    k = ReferenceListProperty(kx, ky)
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            self.touch = touch
+            self.colour = [0.2, 0.8, 0.2]
+    def on_touch_move(self, touch):
+        if touch is self.touch:
+            self.center = touch.pos
+            self.recalculate_k()
+    def on_touch_up(self, touch):
+        if touch is self.touch:
+            self.colour = [0.8, 0.2, 0.2]
+    def recalculate_k(self, *args):
+        dx = self.x - self.parent.center_x
+        dy = self.y - self.parent.center_y
+        length = min(self.parent.width, self.parent.height)
+        self.kx = dx / length * 30 * 3.1416
+        self.ky = dy / length * 30 * 3.1416
+        print 'parent.shader_widget is', self.parent.shader_widget
+        self.parent.shader_widget.on_wavevectors()
 
 class PlaneWaveApp(App):
     def build(self):
